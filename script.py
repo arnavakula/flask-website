d318b39b5a491a90956ff61dcb0e6d12c1a80103
Arnav Akula <akula.arnav@gmail.com>
2020-06-11 17:41:42 -0700

    Enabled navigation

--- script.py
+++ script.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/about/')
-def home():
+def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
+    print("working")
     app.run(debug = True)
